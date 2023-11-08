3cba23f8181b9aafd210a68e2fe07ed9f12f1a5d
parth thakor <parththakor2610@gmail.com>
2023-11-08 12:35:03 +0530

    adding env file data

--- util/database.js
+++ util/database.js
@@ -2,17 +2,10 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT,
 });
 
 module.exports = sequelize;
 
 
-
-// const sequelize = new Sequelize('nodecompleted1', 'root', 'Parth@Sagar26', {
-//   dialect: 'mysql',
-//   host: 'localhost'
-// });
-
-
-// module.exports = sequelize;
